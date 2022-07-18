4290ab39239e176ec03ca6823e19772c1d08617c
Theo <tdgreer1203@gmail.com>
2022-07-18 09:58:04 -0400

    Updated Issues Display

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -12,7 +13,7 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("there was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -53,3 +54,17 @@ var displayWarning = function(repo) {
     linkEl.setAttribute("target", "_blank");
     limitWarningEl.appendChild(linkEl);
 };
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+}
+
+getRepoName();
